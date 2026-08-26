bcf476b89c9b69777b2c3c7a809abdd521d2a921
Abubakar Asif <Abubakarasif202@hotmail.com>
2026-08-26 21:33:22 +1000

    seo: include expanded service areas in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,14 @@
 import { MetadataRoute } from "next";
 import { business, indexablePaths } from "../lib/site-data";
+import { hfServiceAreas } from "../lib/hf-service-areas";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return indexablePaths.map((path) => ({
+  const paths = [
+    ...indexablePaths,
+    ...hfServiceAreas.map((page) => `/areas/${page.slug}`),
+  ];
+
+  return [...new Set(paths)].map((path) => ({
     url: `${business.domain}${path === "/" ? "" : path}`,
   }));
 }
